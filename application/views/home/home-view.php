9fbecc1172bd1beaecb618b718a0cb8650cb5e24
yanaIvanova3 <y.m.ivanova23@gmail.com>
2016-07-09 22:20:29 +0300

    Home view added

---
+++ application/views/home/home-view.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>АГПСМП по кардиология Оранта ООД</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo"></div>
+            <div class="info"></div>
+        </div> <!--emd header-->
+    </div>
+</body>
+</html>
+
