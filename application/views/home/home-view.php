dcb3dbb09ee80d9e726110ceb854ec3642017112
yanaIvanova3 <y.m.ivanova23@gmail.com>
2016-07-09 22:47:29 +0300

    Home view added

--- application/views/home/home-view.php
+++ application/views/home/home-view.php
@@ -9,10 +9,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
     <div class="container">
-        <div class="header">
+        <header>
             <div class="logo"></div>
             <div class="info"></div>
-        </div> <!--emd header-->
+        </header> <!--emd header-->
+        <nav>
+            <ul class="nav-bar">
+                <li class="nav-menu">НАШИЯТ ЕКИП</li>
+                <li class="nav-menu">РАБОТНО ВРЕМЕ</li>
+                <li class="nav-menu">УСЛУГИ</li>
+                <li class="nav-menu">ЦЕНОРАЗПИС</li>
+                <li class="nav-menu"></li>
+                <li class="nav-menu"></li>
+            </ul>
+        </nav>
+        <div class="content">
+
+        </div>
+        <footer>
+            <div class="dev-copyright"></div>
+            <div class="social">
+                <div class="fb"></div>
+                <div class="gm"></div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
